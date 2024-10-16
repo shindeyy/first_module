c295625c90280c8ac52da0aa1a5e753b5ed80b67
Yogesh Shinde <yogesh@fittr.com>
2024-10-16 15:54:09 +0530

    - Pilot commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const FirstModuleScreen());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First Module',
+      home: const FirstModuleScreen(),
+    );
+  }
+}
+
+
+class FirstModuleScreen extends StatelessWidget {
+  const FirstModuleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('First Module')),
+      body: const Center(child: Text('This is the First Module')),
+    );
+  }
+}
